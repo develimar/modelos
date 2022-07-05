f232b4431707869fffb84a59d005f58d12362e39
Prime_Midia <elimar.pc@gmail.com>
2022-07-05 16:23:44 -0300

    Inicio dos estudos com model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    //especifica a tabela a qual devemos trabalhar
+//    protected $table = 'artigos';
+
+    //sempre pega o campo id como primario
+//    protected $primaryKey = 'id';
+
+    //por padrão vem ativado o timestamps - voce pode desabilitar
+//    public $timestamps = true;
+
+    //as duas colunas por padrão podem ser alteradas - dentro da migration é necessario criar na mao as colunas com $table->timestamps('nomes')->nullable()
+//    public const CREATED_AT = "criado_em";
+//    public const UPDATED_AT = "atualizado_em";
+
+
 }
